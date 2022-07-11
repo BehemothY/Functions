b28c248687831ced0fc7861c839b1571c28a175e
Aser <Aser@DESKTOP-A26D6GR>
2022-07-11 16:50:37 +0300

    FunctionsBasics

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -2,9 +2,43 @@
 #include<iostream>
 using namespace std;
 
+int Add(int a, int b);// Прототип функции
+int Sub(int a, int b);
+int Mul(int a, int b);
+double Div(int a, int b);
+
 void main()
 {
 	setlocale(LC_ALL, "");
-
+	int a, b;
+	cout << "Введите два числа:"; cin >> a >> b;
+	int c = Add(a, b);// Использование функции
+	cout << a << "+" << b << "=" << c << endl;
+	cout << a << "-" << b << "=" << Sub(a, b) << endl;
+	cout << a << "*" << b << "=" << Mul(a, b) << endl;
+	cout << a << "/" << b << "=" << Div(a, b) << endl;
 
 }
+
+int Add(int a, int b)// Реализация функции
+{
+	//Addition - Сложение
+	int c = a + b;
+	return c;
+}
+int Sub(int a, int b)
+{
+	//Subtraction - Вычитание
+	return a - b;
+}
+int Mul(int a, int b)
+{
+	//Mul - Multiplication ( умножение )
+	return a * b;
+}
+double Div(int a, int b)
+{
+	//Div - Division ( деление )
+	return (double) a / b;
+}
+
